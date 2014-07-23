45de4b0ecab40df05f98ab5d279817c8e4c618a1
Juan Pedro Lozano <jplozano@yreaction.com>
2014-07-23 04:09:59 +0200

    GamePlay done

---
+++ swiftAnoid/swiftAnoid/EndScene.swift
@@ -0,0 +1,13 @@
+//
+//  EndScene.swift
+//  anoid
+//
+//  Created by Juan Pedro Lozano on 18/07/14.
+//  Copyright (c) 2014 Juan Pedro Lozano. All rights reserved.
+//
+
+import UIKit
+
+class EndScene: SKScene {
+   
+}
